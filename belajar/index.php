16154af03ec6119261fc0b2850a9e7b2f1a30e1a
astridinggit <61198086+astridinggit@users.noreply.github.com>
2020-02-19 00:45:08 +0700

    Penambahan File Index untuk Apa misalnya
    terus ini deskripsinya, boleh ditambahkan boleh tidak.

---
+++ belajar/index.php
@@ -0,0 +1,10 @@
+BTW ini masih native an ya, aku masih belum main laravel wkwk. ini ngerjain pas masih belum kenal laravel soalnya
+mbah belajar laravel kapan sih?
+pas masuk EDII wkwk. cepet amat
+itu baru masuk pertama langsung dikasih kerjaan laravel, langsung belajar. wkwk jadi gitu. oke oke lanjuut.
+agak nyesel juga sih aslinya. kwkw.
+nah sudah. ini kita kan buat file baru, atau bisa dibilang edit projek yang tak undang lewat git kan ?
+sampai isni paham ? yuups. ketika kita save. maka warna disebelah akan berubah. karna ini sifatnya satu projek sama aku.
+ketika inggit sudah melakukan update. selanjutnya adalah push. kerjaan inggit ini ke git. biar nanti aku pull.
+push artinya kerjaan inggit di upload ke git, terus pull artinya aku download kerjaan inggit. jadinya kek gini
+ini kita save ya 
