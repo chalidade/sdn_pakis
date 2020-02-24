c16c0e79d807499bf69f300e34c98f49771377a7
Chalid Ade Rahman <chalidade@gmail.com>
2020-02-24 19:47:57 +0700

    up up

--- admin/app/model/ProfileModel.php
+++ admin/app/model/ProfileModel.php
@@ -6,33 +6,28 @@ include "../config/setting.php";
 $id = $_REQUEST['id'];
 
 switch ($id) {
-  case 'modalMembaca':
-    $imageName = "MEMBACA_COVER";
-    $page = "input_membaca";
+  case 'profileModel':
+    $imageName = "PROFILE_IMAGE";
+    $page = "profil";
     $json = array(
     "action"                   => "simpleSave",
     "db"                       => $database,
-    "table"                    => "tx_hdr_buku_membaca",
-    "primaryKey"               => "MEMBACA_ID",
+    "table"                    => "tx_home_profile",
+    "primaryKey"               => "PROFILE_ID",
     "value"                    => [
     array(
-    "MEMBACA_ID"               =>  $_POST['MEMBACA_ID'],
-    "MEMBACA_COVER"            =>  date("d_m_Y")."_".basename($_FILES[$imageName]["name"]),
-    "MEMBACA_TANGGAL"          =>  $_POST['MEMBACA_TANGGAL'],
-    "MEMBACA_SISWA"            =>  $_POST['MEMBACA_SISWA'],
-    "MEMBACA_GURU"             =>  $_POST['MEMBACA_GURU'],
-    "MEMBACA_JUDUL"            =>  $_POST['MEMBACA_JUDUL'],
-    "MEMBACA_PENGARANG"        =>  $_POST['MEMBACA_PENGARANG'],
-    "MEMBACA_PENERBIT"         =>  $_POST['MEMBACA_PENERBIT'],
-    "MEMBACA_TOKOH"            =>  $_POST['MEMBACA_TOKOH'],
-    "MEMBACA_RANGKUMAN"        =>  $_POST['MEMBACA_RANGKUMAN'],
-    "MEMBACA_SARAN"            =>  $_POST['MEMBACA_SARAN']
+    "PROFILE_ID"               =>  "1",
+    "PROFILE_IMAGE"            =>  date("d_m_Y")."_".basename($_FILES[$imageName]["name"]),
+    "PROFILE_VISI"              =>  $_POST['PROFILE_VISI'],
+    "PROFILE_MISI"             =>  $_POST['PROFILE_MISI'],
+    "PROFILE_INFORMASI"        =>  $_POST['PROFILE_INFORMASI'],
+    "PROFILE_MAPS"             =>  $_POST['PROFILE_MAPS'],
     )]);
 
-    if (!empty($_FILES[$imageName]["name"])) uploadImage($imageName, "Membaca");
+    if (!empty($_FILES[$imageName]["name"])) uploadImage($imageName, "Profile");
 
     break;
-    
+
   default:
     $json = array(
       "ERROR" => "No Format JSON FOUND"
@@ -55,7 +50,7 @@ function uploadImage($input,$folder) {
   // if everything is ok, try to upload file
   } else {
       if (move_uploaded_file($_FILES[$input]["tmp_name"], $target_file)) {
-          // echo "The file ". basename( $_FILES["MEMBACA_COVER"]["name"]). " has been uploaded.";
+          // echo "The file ". basename( $_FILES["PROFILE_IMAGE"]["name"]). " has been uploaded.";
       } else {
           // echo "Sorry, there was an error uploading your file.";
       }
--- admin/view/content/profil.php
+++ admin/view/content/profil.php
@@ -27,10 +27,12 @@
             </div>
             <!-- /.box-header -->
             <div class="box-body">
-              <div class="row content">
+              <form class="" action="app/model/ProfileModel.php?id=profileModel" method="post" enctype="multipart/form-data">
+              <div class="row content" id="profil">
                 <div class="col-md-12">
-                  <label class="container" for="imgSlider1" style="height:200px; width:100%;border:1px solid #d4d4d4;margin-bottom:20px">
-                    <input type="file" id="imgSlider1" name="imgSlider1" value="" style="display:none">
+                  <template  v-for="data in profil">
+                  <label class="container" for="PROFILE_IMAGE" style="height:200px; width:100%;border:1px solid #d4d4d4;margin-bottom:20px">
+                    <input type="file" id="PROFILE_IMAGE" name="PROFILE_IMAGE" v:bind:value="data.PROFILE_IMAGE" style="display:none">
                     <div class="sliderChangePicture" style="border:1px solid;width:100%;margin-top:150px;padding:5px 10px;">
                       <center>
                         <i class="fa fa-camera"></i> <font style="font-weight:100;margin-left:5px;"> Change Picture</font>
@@ -41,39 +43,40 @@
                 <div class="col-md-6" style="margin-top:10px;">
                     <label for="desc" style="width:100%">
                       Visi
-                      <textarea type="text" id="desc" class="form-control" name="" style="height:250px"></textarea>
+                      <textarea type="text" id="desc" class="form-control" name="PROFILE_VISI" style="height:250px">{{ data.PROFILE_VISI }}</textarea>
                     </label>
                 </div>
                 <div class="col-md-6" style="margin-top:10px;">
                     <label for="desc" style="width:100%">
                       Misi
-                      <textarea type="text" id="desc" class="form-control" name="" style="height:250px"></textarea>
+                      <textarea type="text" id="desc" class="form-control" name="PROFILE_MISI" style="height:250px">{{ data.PROFILE_MISI }}</textarea>
                     </label>
                 </div>
                 <div class="col-md-6" style="margin-top:20px;">
                     <label for="desc" style="width:100%">
                       Informasi Sekolah
-                      <textarea type="text" id="desc" class="form-control" name="" style="height:270px"></textarea>
+                      <textarea type="text" id="desc" class="form-control" name="PROFILE_INFORMASI" style="height:270px">{{ data.PROFILE_INFORMASI }}</textarea>
                     </label>
                 </div>
                 <div class="col-md-6" style="margin-top:20px;">
                     <label for="desc" style="width:100%">
                       Maps
-                      <input type="text"  class="form-control" name="" value=""> <br>
+                      <input type="text"  class="form-control" name="PROFILE_MAPS" v-bind:value="data.PROFILE_MAPS"> <br>
                       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.6293869796755!2d112.7198922143711!3d-7.282940773596742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fbf17c39f8d7%3A0x2cad596bbf531e5e!2sSekolah%20Dasar%20Negeri%20Pakis%20V%20Surabaya!5e0!3m2!1sid!2sid!4v1577937045449!5m2!1sid!2sid" width="100%" height="200" style="border-radius:5px" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
                     </label>
                 </div>
                 <div class="col-md-12">
-                  <button type="button" class="btn btn-success" name="button" style="width:100%;margin-top:20px">Simpan</button>
+                  <button type="submit" class="btn btn-success" name="button" style="width:100%;margin-top:20px">Simpan</button>
                 </div>
               </div>
             </div>
+            </template>
+          </form>
             <!-- /.box-body -->
           </div>
           <!-- /.box -->
         </div>
       <!-- End Kata Pengantar -->
-
 
     </div>
 
@@ -83,3 +86,25 @@
   <!-- /.content -->
 </div>
 <!-- /.content-wrapper -->
+
+<script type="text/javascript">
+var url = "<?php echo $urlApi; ?>";
+new Vue({
+    el: '#profil',
+    data () {
+      return {
+        profil: null
+      }
+    },
+    mounted () {
+      axios
+      .post(url+'/index', {
+        action: 'list',
+        db: 'sdnpakis',
+        table: 'tx_home_profile',
+        start: 0
+      })
+      .then(response => (this.profil = response["data"]["result"]))
+    }
+  })
+</script>
