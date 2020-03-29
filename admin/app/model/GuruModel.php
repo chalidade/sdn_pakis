5c5c90f214c287a41fc198e14e1e83c16eaf1a4f
Chalid Ade Rahman <chalidade@gmail.com>
2020-03-29 19:46:32 +0700

    done profile guru

--- admin/app/model/GuruModel.php
+++ admin/app/model/GuruModel.php
@@ -34,10 +34,10 @@ switch ($id) {
             'USER_EMAIL'      => $_POST["USER_EMAIL"],
             'USER_ADDRESS'    => $_POST["USER_ADDRESS"],
             'USER_ROLE'       => $role,
-            'USER_PASSWORD'   => $_POST["DTL_NIP"],
+            'USER_PASSWORD'   => "",
             'USER_BIRTHDATE'  => $_POST["USER_BIRTHDATE"],
             'USER_BIRTHPLACE' => $_POST["USER_BIRTHPLACE"],
-            'USER_NIP'        => $_POST["DTL_NIP"],
+            'USER_NIP'        => $_POST["USER_NIP"],
             'USER_TOKEN'      => '',
             'USER_STATUS'     => '',
             'USER_ACTIVITY'   => '',
@@ -76,6 +76,79 @@ switch ($id) {
     if (!empty($_FILES[$imageName]["name"])) uploadImage($imageName, "User");
 
     break;
+
+    case 'update':
+      $guruId     = $_POST["USER_ID"];
+      $imageName  = "USER_PHOTO";
+      if (!empty($_FILES[$imageName]["name"])) {
+        $photo = date("d_m_Y")."_".basename($_FILES[$imageName]["name"]);
+      } else {
+        $photo = $_POST["USER_PHOTO_BACKUP"];
+      }
+
+      $page  = "detailGuru.php?id=$guruId";
+      $modal = "true";
+      $json = array (
+        'action' => 'saveheaderdetail',
+        'data' =>
+        array (
+        'HEADER','DETAIL'
+        ),
+        'HEADER' =>
+        array (
+          'DB' => $databaseApi,
+          'TABLE' => 'tx_hdr_user',
+          'PK' => 'USER_ID',
+          'VALUE' =>
+          array (
+            array (
+              "USER_ID"         => $guruId,
+              'USER_NAME'       => $_POST["USER_NAME"],
+              'USER_EMAIL'      => $_POST["USER_EMAIL"],
+              'USER_ADDRESS'    => $_POST["USER_ADDRESS"],
+              'USER_ROLE'       => $role,
+              'USER_PASSWORD'   => $_POST["USER_PASSWORD"],
+              'USER_BIRTHDATE'  => $_POST["USER_BIRTHDATE"],
+              'USER_BIRTHPLACE' => $_POST["USER_BIRTHPLACE"],
+              'USER_NIP'        => $_POST["USER_NIP"],
+              'USER_TOKEN'      => '',
+              'USER_STATUS'     => '',
+              'USER_ACTIVITY'   => '',
+              'USER_PHOTO'      => $photo
+            ),
+          ),
+        ),
+        'DETAIL' =>
+        array (
+          'DB' => $databaseApi,
+          'TABLE' => 'tx_dtl_user_guru',
+          'FK' =>
+          array (
+            'DTL_HDR_ID',
+            'USER_ID',
+          ),
+          'VALUE' =>
+          array (
+            array (
+              "DTL_ID"            => "",
+              "DTL_HDR_ID"        => "",
+              "DTL_NUPTK"         => $_POST["DTL_NUPTK"],
+              "DTL_STATUS"        => $_POST["DTL_STATUS"],
+              "DTL_JABATAN"       => $_POST["DTL_JABATAN"],
+              "DTL_IJAZAH"        => $_POST["DTL_IJAZAH"],
+              "DTL_IJAZAH_TAHUN"  => $_POST["DTL_IJAZAH_TAHUN"],
+              "DTL_IJAZAH_JURUSAN"=> $_POST["DTL_IJAZAH_JURUSAN"],
+              "DTL_TANGGAL_MULAI" => $_POST["DTL_TANGGAL_MULAI"],
+              "DTL_GOL"           => $_POST["DTL_GOL"],
+              "DTL_TELPON"        => $_POST["DTL_TELPON"]
+            ),
+          ),
+        ),
+      );
+
+      if (!empty($_FILES[$imageName]["name"])) uploadImage($imageName, "User");
+
+      break;
 
   default:
     $json = array(
