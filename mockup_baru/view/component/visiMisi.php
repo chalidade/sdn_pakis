4ef6ae86f14be65b712685713d2e8287976fa981
Chalid Ade Rahman <chalidade@gmail.com>
2020-03-31 18:34:47 +0700

    Update visiMisi.php

--- mockup_baru/view/component/visiMisi.php
+++ mockup_baru/view/component/visiMisi.php
@@ -1,28 +1,46 @@
-<div class="site-half">
-  <div class="img-bg-1" style="background-image: url('images/img_4.jpg');" data-aos="fade"></div>
+<div class="site-half mb-5" data-aos="fade">
+  <div class="img-bg-1" style="background-image: url('public/images/img_4.jpg');margin-bottom:20px"></div>
   <div class="container">
     <div class="row no-gutters align-items-stretch">
-      <div class="col-lg-5 ml-lg-auto py-5">
-        <span class="caption d-block mb-2 font-secondary font-weight-bold">Outstanding Services</span>
-        <h2 class="site-section-heading text-uppercase font-secondary mb-5">Clean Design</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aliquid eius facilis voluptatem eligendi magnam accusamus vel commodi asperiores sint rem reprehenderit nobis nesciunt veniam qui fugit doloremque numquam quod.</p>
-
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur tempora distinctio ipsam nesciunt recusandae repellendus asperiores amet.</p>
+      <div class="col-lg-6 ml-lg-auto" style="padding-left:20px">
+        <!-- <span class="caption d-block mb-2 font-secondary font-weight-bold">Outstanding Services</span> -->
+        <h2 class="site-section-heading text-uppercase font-secondary mb-3">Visi</h2>
+        <p>"UTAMAKAN" ( Unggul, Trampil, Beriman dan Peduli Lingkungan) </p>
+        <table style="text-align:justify">
+          <tr><td style="vertical-align: top;"width="7%">A. </td><td>Unggul dalam bidang akademik dan Non Akademik </td></tr>
+          <tr><td style="vertical-align: top;">B. </td><td>Unggul dalam bidang Ilmu Pengetahuan dan Teknologi  </td></tr>
+          <tr><td style="vertical-align: top;">C. </td><td>Unggul dalam bidangpekerti yang luhur dan berakhlak mulia  </td></tr>
+          <tr><td style="vertical-align: top;">D. </td><td>Unggul dalam bidang Keagamaan  </td></tr>
+          <tr><td style="vertical-align: top;">E. </td><td>Trampil dalam bidang Akademik dan Non Akademik  </td></tr>
+          <tr><td style="vertical-align: top;">F. </td><td>Trampil dalam bidang Ilmu Pengetahuan dan Teknologi  </td></tr>
+          <tr><td style="vertical-align: top;">G. </td><td> Trampil dalam berintraksi dengan lingkungan </td></tr>
+          <tr><td style="vertical-align: top;">H. </td><td>Trampil dalam beribadah </td></tr>
+          <tr><td style="vertical-align: top;">I. </td><td> Peduli lingkungan sekitar</td></tr>
+        </table>
       </div>
     </div>
   </div>
 </div>
 
-<div class="site-half block">
-  <div class="img-bg-1 right" style="background-image: url('images/img_5.jpg');" data-aos="fade"></div>
+<div class="site-half mb-5" data-aos="fade">
+  <div class="img-bg-1" style="background-image: url('public/images/img_4.jpg');margin-bottom:20px"></div>
   <div class="container">
     <div class="row no-gutters align-items-stretch">
-      <div class="col-lg-5 mr-lg-auto py-5">
-        <span class="caption d-block mb-2 font-secondary font-weight-bold">Easy To Use Templates</span>
-        <h2 class="site-section-heading text-uppercase font-secondary mb-5">Free HTML5 Templates</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus aliquid eius facilis voluptatem eligendi magnam accusamus vel commodi asperiores sint rem reprehenderit nobis nesciunt veniam qui fugit doloremque numquam quod.</p>
-
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur tempora distinctio ipsam nesciunt recusandae repellendus asperiores amet.</p>
+      <div class="col-lg-6 ml-lg-auto" style="padding-left:20px">
+        <!-- <span class="caption d-block mb-2 font-secondary font-weight-bold">Outstanding Services</span> -->
+        <h2 class="site-section-heading text-uppercase font-secondary mb-3">Misi</h2>
+        <p>Merumuskan dan menetapkan misi serta mengembangkannya Misi sebagai berikut</p>
+        <table style="text-align:justify">
+          <tr><td style="vertical-align: top;"width="7%">A. </td><td>Menyelenggarakan pendidikan untuk menghasilkan peserta didik yang unggul dalam bidang akademik dan non akademik </td></tr>
+          <tr><td style="vertical-align: top;">B. </td><td>Menyelenggarakan pendidikan untuk menghasilkan peserta didik yang unggul dalam Ilmu Pengetahuan dan Tehnologi  </td></tr>
+          <tr><td style="vertical-align: top;">C. </td><td>Memberikan bekal pada peserta didik untuk menciptakan karakter berbudi luhur dan berakhlak mulia</td></tr>
+          <tr><td style="vertical-align: top;">D. </td><td>Memberikan pendidikan dan untuk peserta didik agar unggul dalam bidang keagamaan</td></tr>
+          <tr><td style="vertical-align: top;">E. </td><td>Memberikan bekal untuk peserta didik agar memiliki keterampilan dasar dalam bidang Akademik dan Non Akademik</td></tr>
+          <tr><td style="vertical-align: top;">F. </td><td>Memberikan bekal untuk peserta didik agar memiliki keterampilan dasar dalam bidang Ilmu Pengetahuan dan Teknologi</td></tr>
+          <tr><td style="vertical-align: top;">G. </td><td> Menciptakan peserta didik agar trampil dalam berinteraksi dengan lingkungan sekitar</td></tr>
+          <tr><td style="vertical-align: top;">H. </td><td>Membentuk pribadi peserta didik yang berakhlak mulia dengan berlandaskan Iman dan Taqwa kepada Tuhan Yang Maha Esa</td></tr>
+          <tr><td style="vertical-align: top;">I. </td><td>Membentuk pribadi peserta didik yang taat beribadah. Menciptakan suasana peduli lingkungan yang berdasarkan kekeluargaan dan pembiasaan sopan santun (senyum, sapa dan salim)budaya sadar dan tertib hukum dalam lingkungan sekolah</td></tr>
+        </table>
       </div>
     </div>
   </div>
