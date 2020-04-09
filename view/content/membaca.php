158471cbb6748e45ed8fc81025e9f1796412f58a
Chalid Ade Rahman <chalidade@gmail.com>
2020-04-09 15:20:16 +0700

    rangking

--- view/content/membaca.php
+++ view/content/membaca.php
@@ -31,36 +31,39 @@
         $start = 0;
         $page  = 1;
       }
-      $count   = json_decode(json_encode(mysqli_fetch_assoc(mysqli_query($mysqli, "SELECT count(DISTINCT MEMBACA_SISWA) as TOTAL_DATA FROM `tx_hdr_buku_membaca`"))), TRUE);
+      $count   = json_decode(json_encode(mysqli_fetch_assoc(mysqli_query($mysqli, "SELECT count(DISTINCT DTL_NIS) as TOTAL_DATA FROM `tx_dtl_user_siswa`"))), TRUE);
       $count   = $count["TOTAL_DATA"];
       $limit   = 25;
       $pagi    = ceil($count/$limit);
       $sql     = "
-                 SELECT COUNT(A.MEMBACA_SISWA) AS total, B.*, C.*
-                 FROM tx_hdr_buku_membaca as A
-                 JOIN tx_dtl_user_siswa as B ON B.DTL_NIS = A.MEMBACA_SISWA
+                 SELECT *
+                 FROM tx_dtl_user_siswa as B
                  JOIN tx_hdr_user as C ON B.DTL_HDR_ID = C.USER_ID
-                 GROUP BY A.MEMBACA_SISWA
-                 ORDER BY total DESC
                  LIMIT $start, $limit
                  ";
        $query  = mysqli_query($mysqli, $sql);
        $no     = $start+1;
        while ($siswa = mysqli_fetch_array($query)) {
-         if (!empty($siswa["total"])) {
-           $bercerita   = $siswa["total"];
+        $sqlBaca    = json_decode(json_encode(mysqli_fetch_assoc(mysqli_query($mysqli, "SELECT count('MEMBACA_SISWA') as TOTAL_MEMBACA FROM tx_hdr_buku_membaca WHERE MEMBACA_SISWA = ".$siswa['DTL_NIS'].""))), TRUE);
+        $sqlCerita  = json_decode(json_encode(mysqli_fetch_assoc(mysqli_query($mysqli, "SELECT * FROM tx_hdr_buku_bercerita WHERE BERCERITA_NIS = ".$siswa['DTL_NIS'].""))), TRUE);
+        $sqlTahu    = json_decode(json_encode(mysqli_fetch_assoc(mysqli_query($mysqli, "SELECT count('PENGETAHUAN_NIS') as TOTAL_PENGETAHUAN FROM tx_hdr_buku_pengetahuan WHERE PENGETAHUAN_STATUS = '2' AND PENGETAHUAN_NIS = ".$siswa['DTL_NIS'].""))), TRUE);
+
+        $jmlCerita  = $sqlCerita["BERCERITA_JAN"]+$sqlCerita["BERCERITA_FEB"]+$sqlCerita["BERCERITA_MAR"]+$sqlCerita["BERCERITA_APR"]+$sqlCerita["BERCERITA_MEI"]+$sqlCerita["BERCERITA_JUN"]+$sqlCerita["BERCERITA_JUL"]+$sqlCerita["BERCERITA_AUG"]+$sqlCerita["BERCERITA_SEP"]+$sqlCerita["BERCERITA_OKT"]+$sqlCerita["BERCERITA_NOV"]+$sqlCerita["BERCERITA_DES"];
+
+         if (!empty($sqlBaca["TOTAL_MEMBACA"])) {
+           $bercerita   = $sqlBaca["TOTAL_MEMBACA"];
          } else {
            $bercerita   = 0;
          }
 
-         if (!empty($siswa["menulis"])) {
-           $menulis   = $siswa["menulis"];
+         if (!empty($jmlCerita)) {
+           $menulis   = $jmlCerita;
          } else {
            $menulis   = 0;
          }
 
-         if (!empty($siswa["kip"])) {
-           $kip   = $siswa["kip"];
+         if (!empty($sqlTahu["TOTAL_PENGETAHUAN"])) {
+           $kip   = $sqlTahu["TOTAL_PENGETAHUAN"];
          } else {
            $kip   = 0;
          }
@@ -69,16 +72,19 @@
        ?>
         <tr>
           <?php if ($no > 3) { ?>
-            <td style="vertical-align:top;width:10%"><h3 style="text-align:center"><?php echo $no.".";$no++; ?></h3></td>
+            <td style="vertical-align:top;width:10%"><h4 style="text-align:center"><?php echo $no.".";$no++; ?></h4></td>
           <?php } else { ?>
-            <td style="vertical-align:top;width:10%"><h1 style="text-align:center"><?php echo $no.".";$no++; ?></h1></td>
+            <td style="vertical-align:top;width:10%;">
+              <img src="public/images/rate-star.gif" alt="" width="100%" style="z-index:1;margin-left:40px">
+                <h1 style="text-align:center;font-size:42px;margin-top:-50px"><?php echo $no."";$no++; ?></h1>
+            </td>
           <?php } ?>
           <td style="vertical-align:top;width:10%">
             <center>
-              <img onerror="this.onerror=null; this.src='../img/unavailable.png'" src="admin/resource/public/USER/<?php echo $siswa["USER_PHOTO"]; ?>" class="img-fluid mb-4" style="border-radius:50px;width:60px;height: 60px;"></td>
+              <img onerror="this.onerror=null; this.src='public/images/user.png'" src="admin/resource/public/USER/<?php echo $siswa["USER_PHOTO"]; ?>" class="img-fluid mb-4" style="border-radius:50px;width:60px;height: 60px;"></td>
             </center>
           <td  style="vertical-align:top;color:#000;"><b><?php echo $siswa["USER_NAME"]; ?></b><br><font style="font-size:14px"><?php echo $siswa["DTL_NIS"]; ?></font></td>
-          <td style="text-align:center"><?php echo $siswa["DTL_TINGKAT"]."-".$siswa["DTL_KELAS"]; ?></td>
+          <td style="text-align:center"><?php echo $siswa["DTL_TINGKAT"]." ".$siswa["DTL_KELAS"]; ?></td>
           <td style="text-align:center"><?php echo $bercerita; ?></td>
           <td style="text-align:center"><?php echo $menulis; ?></td>
           <td style="text-align:center"><?php echo $kip; ?></td>
